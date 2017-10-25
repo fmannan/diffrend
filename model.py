f70cf9d6b6e289d6afb2cbbd76893ce61b47c33b
Fahim Mannan <fmannan@gmail.com>
2017-10-25 10:05:30 -0400

    Added data and model loader

---
+++ model.py
@@ -0,0 +1,195 @@
+import numpy as np
+
+
+def norm_sqr(v):
+    return np.sum(v ** 2, axis=-1)
+
+
+def norm(v):
+    return np.sqrt(norm_sqr(v))
+
+
+def compute_face_normal(obj, unnormalized=False):
+    v0 = obj['v'][obj['f'][:, 0]]
+    v1 = obj['v'][obj['f'][:, 1]]
+    v2 = obj['v'][obj['f'][:, 2]]
+
+    v01 = v1 - v0
+    v02 = v2 - v0
+
+    n = np.cross(v01, v02)
+    if unnormalized:
+        return n
+    denom = norm(n)[..., np.newaxis]
+    denom[denom == 0] = 1
+    return n / denom
+
+
+def compute_circum_circle(obj):
+    """
+    https://en.wikipedia.org/wiki/Circumscribed_circle#Cartesian_coordinates_from_cross-_and_dot-products
+    :param obj:
+    :return: {'center': circle_center, 'radius': circle_radius}
+    """
+    p1 = obj['v'][obj['f'][:, 0]]
+    p2 = obj['v'][obj['f'][:, 1]]
+    p3 = obj['v'][obj['f'][:, 2]]
+
+    p23 = p2 - p3
+    p13 = p1 - p3
+    p12 = p1 - p2
+
+    n = np.cross(p12, p23)
+    n_norm_sqr = norm_sqr(n)
+
+    radius = norm(p12) * norm(p23) * norm(-p13) / (2 * np.sqrt(n_norm_sqr))
+
+    denom_inv = 1 / (2 * n_norm_sqr)
+    alpha = norm_sqr(p23) * np.sum(p12 * p13, axis=-1) * denom_inv
+    beta = norm_sqr(p13) * np.sum(-p12 * p23, axis=-1) * denom_inv
+    gamma = norm_sqr(p12) * np.sum(-p13 * -p23, axis=-1) * denom_inv
+
+    center = alpha[:, np.newaxis] * p1 + beta[:, np.newaxis] * p2 + gamma[:, np.newaxis] * p3
+
+    return {'center': center, 'radius': radius}
+
+
+def obj_to_splat(obj, use_circum_circle=True):
+    if not use_circum_circle:
+        raise ValueError('obj_to_splat only support circumscribed circle for now.')
+    cc = compute_circum_circle(obj)
+    normals = compute_face_normal(obj)
+    return {'v': cc['center'], 'r': cc['radius'], 'vn': normals}
+
+
+def write_splat(filename, obj):
+    with open(filename, 'w') as f:
+        for v, vn, r in zip(obj['v'], obj['vn'], obj['r']):
+            f.write('v {}\n'.format(' '.join([str(x) for x in v])))
+            f.write('vn {}\n'.format(' '.join([str(x) for x in vn])))
+            if type(r) is np.ndarray or type(r) is list:
+                f.write('r {}\n'.format(' '.join([str(x) for x in r])))
+            else:
+                f.write('r {}\n'.format(r))
+
+
+def load_splat(filename):
+    with open(filename, 'r') as f:
+        obj_file = f.read().splitlines()
+
+    v = []  # list of vertices
+    vn = []  # list of vertex normals
+    r = []   # list of splat radii single value means disk, pair ellipse, triple ellipsoid/sphere
+
+    for line in obj_file:
+        line = line.split(' ')
+        if line[0] == '#':
+            continue
+        if line[0] == 'v':
+            v.append([float(x) for x in line[1:]])
+        if line[0] == 'vn':
+            vn.append([float(x) for x in line[1:]])
+        if line[0] == 'r':
+            r.append([float(x) for x in line[1:]])
+
+    print('Vertex count: {}'.format(len(v)))
+    print('Vertex Normal count: {}'.format(len(vn)))
+    print('Splat Radius count: {}'.format(len(r)))
+
+    return {'v': np.array(v), 'vn': np.array(vn), 'r': np.array(r)}
+
+
+def load_obj(filename):
+    with open(filename, 'r') as f:
+        obj_file = f.read().splitlines()
+
+    #print(obj_file)
+    v = []  # list of vertices
+    f = []  # list of faces
+
+    for line in obj_file:
+        line = line.split(' ')
+        if line[0] == '#':
+            continue
+        if line[0] == 'v':
+            v.append([float(x) for x in line[1:]])
+        if line[0] == 'f':  # Note the conversion from 1-based to 0-based index
+            f.append([(int(x) - 1) for x in line[1:]])
+
+    print('Vertex count: {}'.format(len(v)))
+    print('Face count: {}'.format(len(f)))
+
+    return {'v': np.array(v), 'f': np.array(f)}
+
+
+def load_off(filename):
+    with open(filename, 'r') as f:
+        obj_file = f.read().splitlines()
+
+    v = []  # list of vertices
+    f = []  # list of faces
+    e = []  # list of edges
+
+    num_vertices = None
+    num_faces = None
+    num_edges = None
+    check_num_vertex_face = False
+
+    for line in obj_file:
+        line = line.split(' ')
+        if line[0] == 'OFF':
+            check_num_vertex_face = True
+            if len(line) > 1:
+                num_vertices, num_faces, num_edges = int(line[1]), int(line[2]), int(line[3])
+                check_num_vertex_face = False
+            continue
+        elif check_num_vertex_face:
+            num_vertices, num_faces, num_edges = int(line[0]), int(line[1]), int(line[2])
+            check_num_vertex_face = False
+            continue
+
+        if len(v) < num_vertices:
+            v.append([float(x) for x in line])
+        elif len(f) < num_faces:
+            f.append([int(x) for x in line[1:]])
+        elif len(e) < num_edges:
+            e.append([int(x) for x in line])
+
+    print('#V: {}, #F: {}, #E: {}'.format(num_vertices, num_faces, num_edges))
+
+    print('Vertex count: {}'.format(len(v)))
+    print('Face count: {}'.format(len(f)))
+    print('Edge count: {}'.format(len(e)))
+
+    return {'v': np.array(v), 'f': np.array(f), 'e': np.array(e)}
+
+
+def obj_to_triangle_spec(obj, material):
+    faces = obj['v'][obj['f']]
+    normals = compute_face_normal(obj)
+    faces = np.concatenate((faces, np.ones_like(faces[..., 0])[..., np.newaxis]), axis=-1)
+    normals = np.concatenate((normals, np.zeros_like(normals[..., 0])[..., np.newaxis]), axis=-1)
+
+    return {'face': faces, 'normal': normals, 'material': np.array(material)}
+
+
+def test_run():
+    filename = './data/bunny.obj'
+    obj_data = load_obj(filename)
+
+    cc = compute_circum_circle(obj_data)
+
+    filename = './data/desk_0007.off'
+    off_data = load_off(filename)
+
+    return obj_data, off_data
+
+
+if __name__ == '__main__':
+    obj_data, off_data = test_run()
+    obj_splat = obj_to_splat(obj_data)
+    write_splat('test.splat', obj_splat)
+    splat_obj = load_splat('test.splat')
+    tri_bunny = obj_to_triangle_spec(obj_data, material=[0.8, 0.8, 0.8])
+    tri_desk = obj_to_triangle_spec(off_data, material=[0.5, 0.5, 0.8])
+
