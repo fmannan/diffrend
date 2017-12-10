1e48094a57bab334e381d7163d53fd0abbdf5bbb
Sai Rajeswar Mudumba <mudumbas@leto36.iro.umontreal.ca>
2017-12-10 13:50:46 -0500

    higher batch_size doesn't work

--- diffrend/torch/GAN/generator.py
+++ diffrend/torch/GAN/generator.py
@@ -33,13 +33,13 @@ parser.add_argument('--model', type=str, default=DIR_DATA + '/chair_0001.off')
 parser.add_argument('--out_dir', type=str, default='./render_samples/')
 parser.add_argument('--width', type=int, default=128)
 parser.add_argument('--height', type=int, default=128)
-parser.add_argument('--n', type=int, default=5000)
+parser.add_argument('--n', type=int, default=2000)
 parser.add_argument('--r', type=float, default=0.025)
 parser.add_argument('--cam_dist', type=float, default=5.0, help='Camera distance from the center of the object')
 parser.add_argument('--nv', type=int, default=10, help='Number of views to generate')
 parser.add_argument('--fovy', type=float, default=15.0, help='Field of view in the vertical direction')
 parser.add_argument('--f', type=float, default=0.1, help='focal length')
-
+parser.add_argument('--same_view', action='store_true', default=False, help='data with view fixed')
 args = parser.parse_args()
 
 def same_view(filename,  num_samples, radius, width, height,fovy, focal_length,cam_pos,batch_size):
@@ -99,7 +99,60 @@ def same_view(filename,  num_samples, radius, width, height,fovy, focal_length,c
         data.append(im_depth.unsqueeze(0))
     return torch.stack(data)
 
+def different_views(filename,  num_samples, radius, cam_dist,  width, height,fovy, focal_length,batch_size):
+    """
+    Randomly generate N samples on a surface and render them. The samples include position and normal, the radius is set
+    to a constant.
+    """
+    obj = load_model(filename)
 
+
+    r = np.ones(num_samples) * radius
+
+    large_scene = copy.deepcopy(SCENE_BASIC)
+
+    large_scene['camera']['viewport'] = [0, 0, width, height]
+    large_scene['camera']['fovy'] = np.deg2rad(fovy)
+    large_scene['camera']['focal_length'] = focal_length
+    large_scene['objects']['disk']['radius'] = tch_var_f(r)
+    large_scene['objects']['disk']['material_idx'] = tch_var_l(np.zeros(num_samples, dtype=int).tolist())
+    large_scene['materials']['albedo'] = tch_var_f([[0.6, 0.6, 0.6]])
+    large_scene['tonemap']['gamma'] = tch_var_f([1.0])  # Linear output
+
+    # generate camera positions on a sphere
+    cam_pos = uniform_sample_sphere(radius=cam_dist, num_samples=batch_size)
+    data=[]
+    for idx in range(batch_size):
+        v, vn = uniform_sample_mesh(obj, num_samples=num_samples)
+
+        # normalize the vertices
+        v = (v - np.mean(v, axis=0)) / (v.max() - v.min())
+
+        large_scene['objects']['disk']['pos'] = tch_var_f(v)
+        large_scene['objects']['disk']['normal'] = tch_var_f(vn)
+
+        large_scene['camera']['eye'] = tch_var_f(cam_pos[idx])
+        suffix = '_{}'.format(idx)
+
+        # main render run
+        res = render(large_scene)
+        if CUDA:
+            im = res['image']
+        else:
+            im = res['image']
+
+        if CUDA:
+            depth = res['depth']
+        else:
+            depth = res['depth']
+
+        cond = depth >= large_scene['camera']['far']
+        depth = where(cond, torch.min(depth), depth)
+        #depth[depth >= large_scene['camera']['far']] = torch.min(depth)
+        im_depth =(depth - torch.min(depth)) / (torch.max(depth) - torch.min(depth))
+
+        data.append(im_depth.unsqueeze(0))
+    return torch.stack(data)
 
 
 print(args)
@@ -138,7 +191,8 @@ fixed_noise = Variable(fixed_noise)
 # Setup optimizer
 optimizerD = optim.Adam(netD.parameters(), lr=opt.lr, betas=(opt.beta1, 0.999))
 optimizerG = optim.Adam(netG.parameters(), lr=opt.lr, betas=(opt.beta1, 0.999))
-cam_pos = uniform_sample_sphere(radius=args.cam_dist, num_samples=2)
+if args.same_view:
+    cam_pos = uniform_sample_sphere(radius=args.cam_dist, num_samples=2)
 r = np.ones(args.n) * args.r
 large_scene = copy.deepcopy(SCENE_BASIC)
 large_scene['camera']['viewport'] = [0, 0, args.width,args.height]
@@ -148,7 +202,7 @@ large_scene['objects']['disk']['radius'] = tch_var_f(r)
 large_scene['objects']['disk']['material_idx'] = tch_var_l(np.zeros(args.n, dtype=int).tolist())
 large_scene['materials']['albedo'] = tch_var_f([[0.6, 0.6, 0.6]])
 large_scene['tonemap']['gamma'] = tch_var_f([1.0])  # Linear output
-large_scene['camera']['eye'] = tch_var_f(cam_pos[0])
+#large_scene['camera']['eye'] = tch_var_f(cam_pos[0])
 for epoch in range(opt.niter):
 
     ############################
@@ -156,7 +210,10 @@ for epoch in range(opt.niter):
     ###########################
     # train with real
     netD.zero_grad()
-    real_cpu = same_view(args.model, args.n, args.r,  args.width, args.height, args.fovy, args.f,cam_pos[0],opt.batchSize)
+    if args.same_view:
+        real_cpu = same_view(args.model, args.n, args.r,  args.width, args.height, args.fovy, args.f,cam_pos[0],opt.batchSize)
+    else:
+        real_cpu = different_views(args.model, args.n, args.r,  args.cam_dist,args.width, args.height, args.fovy, args.f,opt.batchSize)
 
 
     batch_size = real_cpu.size(0)
@@ -184,6 +241,7 @@ for epoch in range(opt.niter):
     # generate camera positions on a sphere
 
     data=[]
+    cam_pos = uniform_sample_sphere(radius=args.cam_dist, num_samples=batch_size)
     for idx in range(batch_size):
 
         #import ipdb; ipdb.set_trace()
@@ -192,6 +250,7 @@ for epoch in range(opt.niter):
 
         large_scene['objects']['disk']['pos'] = temp
         large_scene['objects']['disk']['normal'] = fake[idx][:, 3:]
+        large_scene['camera']['eye'] = tch_var_f(cam_pos[idx])
 
 
         suffix = '_{}'.format(idx)
@@ -236,12 +295,12 @@ for epoch in range(opt.niter):
     optimizerG.step()
     suffix = '_{}'.format(epoch)
     if epoch % 10 == 0:
-        print('[%d/%d] Loss_D: %.4f Loss_G: %.4f D(x): %.4f'
+        print('\n[%d/%d] Loss_D: %.4f Loss_G: %.4f D(x): %.4f'
               ' D(G(z)): %.4f / %.4f' % (epoch, opt.niter,
 
                                          errD.data[0], errG.data[0], D_x,
-                                         D_G_z1, D_G_z2), end="\r")
-    if epoch % 100 == 0:
+                                         D_G_z1, D_G_z2))
+    if epoch % 25 == 0:
         imsave(args.out_dir + '/img' + suffix + '.png', np.uint8(255. *real_cpu[0].cpu().data.numpy().squeeze()))
         imsave(args.out_dir + '/img_depth' + suffix + '.png', np.uint8(255. * data[0].cpu().data.numpy().squeeze()))
         imsave(args.out_dir + '/img1' + suffix + '.png', np.uint8(255. *real_cpu[1].cpu().data.numpy().squeeze()))
