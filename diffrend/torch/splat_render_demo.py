lookat_rot_inv axis normalization (bug fix for rendering).

diffrend/torch/splat_render_demo.py
@@ -1,5 +1,5 @@
 from diffrend.torch.params import SCENE_BASIC
-from diffrend.torch.utils import tch_var_f, tch_var_l, CUDA
+from diffrend.torch.utils import tch_var_f, tch_var_l, get_data
 from diffrend.torch.renderer import render
 from diffrend.utils.sample_generator import uniform_sample_mesh, uniform_sample_sphere
 from diffrend.model import load_model
@@ -14,7 +14,7 @@ from scipy.misc import imsave
 
 
 def render_random_splat_camera(filename, out_dir, num_samples, radius, cam_dist, num_views, width, height,
-                               fovy, focal_length, norm_depth_image_only):
+                               fovy, focal_length, norm_depth_image_only, cam_pos=None):
     """
     Randomly generate N samples on a surface and render them. The samples include position and normal, the radius is set
     to a constant.
@@ -45,11 +45,10 @@ def render_random_splat_camera(filename, out_dir, num_samples, radius, cam_dist,
     large_scene['tonemap']['gamma'] = tch_var_f([1.0])  # Linear output
 
     # generate camera positions on a sphere
-    cam_pos = uniform_sample_sphere(radius=cam_dist, num_samples=num_views)
+    if cam_pos is None:
+        cam_pos = uniform_sample_sphere(radius=cam_dist, num_samples=num_views)
     obj_center = np.mean(v, axis=0)
     large_scene['camera']['at'] = tch_var_f(obj_center)
-    # TODO: Change the camera position so that the entire object is inside the view frustum
-    # ...
 
     plt.figure()
     for idx in range(cam_pos.shape[0]):
@@ -68,15 +67,8 @@ def render_random_splat_camera(filename, out_dir, num_samples, radius, cam_dist,
         res = render(large_scene, norm_depth_image_only=norm_depth_image_only)
         rendering_time.append(time() - start_time)
 
-        if CUDA:
-            im = res['image'].cpu().data.numpy()
-        else:
-            im = res['image'].data.numpy()
-
-        if CUDA:
-            depth = res['depth'].cpu().data.numpy()
-        else:
-            depth = res['depth'].data.numpy()
+        im = get_data(res['image'])
+        depth = get_data(res['depth'])
 
         depth[depth >= large_scene['camera']['far']] = depth.min()
         im_depth = np.uint8(255. * (depth - depth.min()) / (depth.max() - depth.min()))
@@ -86,14 +78,37 @@ def render_random_splat_camera(filename, out_dir, num_samples, radius, cam_dist,
 
         plt.imshow(im_depth)
         plt.title('Depth Image')
-        plt.savefig(out_dir + '/fig_img_depth' + suffix + '.png')
+        plt.savefig(out_dir + '/fig_depth' + suffix + '.png')
 
         imsave(out_dir + '/img' + suffix + '.png', im)
-        imsave(out_dir + '/img_depth' + suffix + '.png', im_depth)
+        imsave(out_dir + '/depth' + suffix + '.png', im_depth)
 
     # Timing statistics
     print('Sampling time mean: {}s, std: {}s'.format(np.mean(sampling_time), np.std(sampling_time)))
     print('Rendering time mean: {}s, std: {}s'.format(np.mean(rendering_time), np.std(rendering_time)))
+
+
+def preset_cam_pos_0():
+    return np.array([[3.4065832, -1.26789198, 4.77364021],
+                     [1.73761297, -4.62854391, -3.39960033],
+                     [-2.03811183, 1.50594331, 5.43858758],
+                     [-1.14525852, 5.44444109, -2.24642919],
+                     [-3.72136062, -4.70636702, 0.03980693],
+                     [-2.16651127, 3.45342292, 4.40228339],
+                     [-3.39866041, 1.95044609, -4.54366234],
+                     [-1.80005058, -5.62148377, 1.07644697],
+                     [2.74616603, -1.86863041, 4.99667815],
+                     [-5.78391582, -1.46707216, 0.62770774],
+                     [-1.56162523, -5.78793779, 0.24718981],
+                     [-1.29602688, 4.76694896, -3.40536517],
+                     [-2.49890985, -5.27582671, -1.38603826],
+                     [4.6751337, -3.44721669, -1.50327042],
+                     [0.76567273, 5.60232432, -2.00691493],
+                     [0.55733763, -3.1450544, 5.07917391],
+                     [3.61504468, 4.28476875, -2.13827237],
+                     [-4.1440199, 4.3368818, 0.13621782],
+                     [0.299033, -0.18254953, -5.98976251],
+                     [-1.52430796, -4.79467686, 3.26918324]])
 
 
 if __name__ == '__main__':
@@ -107,18 +122,24 @@ if __name__ == '__main__':
     parser.add_argument('--height', type=int, default=128, help='Height of output image.')
     parser.add_argument('--n', type=int, default=5000, help='Number of samples to generate.')
     parser.add_argument('--r', type=float, default=0.025, help='Constant radius for each splat.')
-    parser.add_argument('--cam_dist', type=float, default=7.0, help='Camera distance from the center of the object.')
+    parser.add_argument('--cam_dist', type=float, default=5.0, help='Camera distance from the center of the object.')
     parser.add_argument('--nv', type=int, default=10, help='Number of views to generate.')
-    parser.add_argument('--fovy', type=float, default=20.0, help='Field of view in the vertical direction.')
+    parser.add_argument('--fovy', type=float, default=18.0, help='Field of view in the vertical direction.')
     parser.add_argument('--f', type=float, default=0.1, help='Focal length of camera.')
     parser.add_argument('--norm_depth_image_only', action='store_true', default=False, help='Render on the normalized'
                                                                                             ' depth image.')
+    parser.add_argument('--test_cam_dist', action='store_true', help='Check if the images are consistent with a'
+                                                                     'camera at a fixed distance.')
 
     args = parser.parse_args()
     print(args)
+
+    cam_pos = None
+    if args.test_cam_dist:
+        cam_pos = preset_cam_pos_0()
 
     render_random_splat_camera(filename=args.model, out_dir=args.out_dir, radius=args.r, num_samples=args.n,
                                cam_dist=args.cam_dist, num_views=args.nv,
                                width=args.width, height=args.height,
                                fovy=args.fovy, focal_length=args.f,
-                               norm_depth_image_only=args.norm_depth_image_only)
+                               norm_depth_image_only=args.norm_depth_image_only, cam_pos=cam_pos)

diffrend/torch/utils.py
@@ -297,7 +297,7 @@ def lookat_rot_inv(eye, at, up):
     z = normalize(z)
 
     up = normalize(up)
-    x = torch.cross(up, z)
+    x = normalize(torch.cross(up, z))
     # The input `up` vector may not be orthogonal to z.
     y = torch.cross(z, x)
 