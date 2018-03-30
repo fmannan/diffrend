5adcb1f0b0f87b39b4345bfcc76844882c366011
Fahim Mannan <fmannan@gmail.com>
2018-03-30 10:18:27 -0400

    [WIP] Visualizer

--- viz/visualizer.py
+++ viz/visualizer.py
@@ -21,24 +21,31 @@ import matplotlib.pyplot as plt
 #     # plt.figure()
 #     # plt.imshow(depth)
 
-data = np.load('res_world.npy')
+data = np.load('res_world_twogans.npy')
 
-pos0 = get_data(data[0]['pos'])
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-ax.scatter(pos0[:, 0], pos0[:, 1], pos0[:, 2], s=1.3)
+# pos0 = get_data(data[0]['pos'])
+# fig = plt.figure()
+# ax = fig.add_subplot(111, projection='3d')
+# ax.scatter(pos0[:, 0], pos0[:, 1], pos0[:, 2], s=1.3)
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 for idx in range(0, len(data), 20):
-    pos = get_data(data[idx]['pos'])
+    if type(data[idx]['pos']) is np.ndarray:
+        pos = data[idx]['pos']
+    else:
+        pos = get_data(data[idx]['pos'])
     ax.scatter(pos[:, 0], pos[:, 1], pos[:, 2], s=1.3)
 
 
 filename_prefix = 'test'
 
 for idx in range(0, len(data), 20):
-    pos = get_data(data[idx]['pos'])[:, :3]
+    if type(data[idx]['pos']) is np.ndarray:
+        pos = data[idx]['pos']
+    else:
+        pos = get_data(data[idx]['pos'])
+    pos = pos[:, :3]
     with open(filename_prefix + '_{:05d}.xyz'.format(idx), 'w') as fid:
         for sub_idx in range(pos.shape[0]):
             fid.write('{}\n'.format(' '.join([str(x) for x in pos[sub_idx]])))
