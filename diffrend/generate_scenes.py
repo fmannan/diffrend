98a3c15fab2c8e84eebefa8c20fb07f81bd298d2
Jerome Parent-Levesque <jerome-pl2@hotmail.ca>
2019-02-01 12:17:44 -0500

    Add basic Cube scene and random object rotations

--- diffrend/generate_scenes.py
+++ diffrend/generate_scenes.py
@@ -5,11 +5,12 @@ import os
 import numpy as np
 
 from diffrend.utils.sample_generator import uniform_sample_sphere
+from diffrend.numpy.ops import normalize as np_normalize
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--template', type=str, default='../scenes/basic_multiobj.json',
+    parser.add_argument('--template', type=str, default='../scenes/basic_cube.json',
                         help='Path to the base scene json file')
     parser.add_argument('--out', type=str, default='./generated_scenes',
                         help='Directory to output the scene json files')
@@ -21,6 +22,7 @@ def main():
                         default=[20, 80], help='Range of angles (in degrees) from the z-axis.')
     parser.add_argument('--phi', nargs=2, type=float,
                         default=[20, 70], help='Range of angles (in degrees) from the x-axis.')
+    parser.add_argument('--axis', nargs=3, default=[0, 1, 0], type=float, help='Axis for random object rotations.')
 
     args = parser.parse_args()
 
@@ -33,7 +35,9 @@ def main():
         scene = json.load(f)
 
     for i in range(args.number):
-        print(i)
+        if (i + 1) % 100 == 0:
+            print(f"Progress: {(i + 1) / args.number}%")
+            
         # Generate a random camera eye position
         camera_pos = uniform_sample_sphere(radius=args.cam_dist, num_samples=1,
                                            theta_range=np.deg2rad(args.theta),
@@ -44,9 +48,19 @@ def main():
         light_pos = uniform_sample_sphere(radius=args.cam_dist, num_samples=1,
                                           theta_range=np.deg2rad(args.theta),
                                           phi_range=np.deg2rad(args.phi))[0]
-        # TODO double check: it seems like only the first light position is randomly generated (see gan.py line 310)
-        # and that only the first 3 values of the 4-value vector are edited (what is this 4th value?)
+        # Only change the first light
         scene['lights']['pos'][0][:3] = light_pos.tolist()
+
+        # Randomly rotate objects that are not the background:
+        for obj in scene['objects']['obj']:
+            # TODO this is pretty hacky, figure out a better way to do this
+            if obj['path'] == "./objs/halfbox.obj":  # Ignore background objects
+                continue
+
+            random_axis = np_normalize(args.axis)
+            random_angle = np.random.rand(1)[0] * np.pi * 2
+            obj['rotate'] = dict(angle_deg=random_angle,
+                                 axis=random_axis.tolist())
 
         # Output the generated scene json file
         with open(f'{args.out}/scene{str(i).zfill(math.ceil(math.log10(args.number)))}.json', 'w') as outfile:
