727eca9205695dd9266daaca764e3365874d5578
Fahim Mannan <fmannan@gmail.com>
2019-03-11 22:13:25 -0400

    model transformation and example scenes

--- diffrend/torch/render.py
+++ diffrend/torch/render.py
@@ -1,7 +1,37 @@
 from diffrend.torch.utils import tch_var_f, tch_var_l, get_data
 from diffrend.torch.renderer import render
 from diffrend.model import load_obj, obj_to_triangle_spec
+from diffrend.utils.utils import get_param_value
+from diffrend.numpy.ops import axis_angle_matrix
 import numpy as np
+
+
+def transform_model(obj, scale, rotate, translate):
+    """Order of transformation is: scale -> rotate -> translate
+    i.e., M = translate * rotate * scale
+
+    Args:
+        obj:
+        scale:
+        rotate:
+        translate:
+
+    Returns:
+
+    """
+    v = obj['v']
+    if scale is not None:
+        v = v * np.array(scale)[np.newaxis, :]
+    if rotate is not None:
+        rotation_axis = rotate['axis']
+        angle_deg = rotate['angle_deg']
+        M = axis_angle_matrix(axis=rotation_axis, angle=np.deg2rad(angle_deg))
+        v = np.matmul(v, M.transpose(1, 0)[:3, :3])
+    if translate is not None:
+        v = v + np.array(translate)[np.newaxis, :]
+
+    obj['v'] = v
+    return obj
 
 
 def load_scene(scene_filename):
@@ -29,6 +59,10 @@ def load_scene(scene_filename):
         model_path = os.path.join(basedir, obj['path'])
         print(os.path.exists(model_path))
         obj_model = load_obj(model_path)
+        scale = get_param_value('scale', obj, None)
+        rotate = get_param_value('rotate', obj, None)
+        translate = get_param_value('translate', obj, None)
+        obj_model = transform_model(obj_model, scale, rotate, translate)
         meshes = obj_to_triangle_spec(obj_model)
         material_idx = np.ones(meshes['face'].shape[0]) * obj['material_idx']
         if mesh['face'] is None:
@@ -66,6 +100,12 @@ def make_torch_var(var_dict):
     return var_dict
 
 
+def render_scene(scene_file):
+    scene = load_scene(scene_file)
+    scene = make_torch_var(scene)
+    return render(scene)
+
+
 def main():
     import argparse
     parser = argparse.ArgumentParser(usage="render.py --scene scene_filename --out_dir output_dir")
@@ -74,10 +114,7 @@ def main():
     args = parser.parse_args()
     print(args)
 
-    scene = load_scene(args.scene)
-
-    scene = make_torch_var(scene)
-    res = render(scene)
+    res = render_scene(args.scene)
     img = res['image'].cpu().numpy().squeeze()
     depth = res['depth'].cpu().numpy().squeeze()
 
